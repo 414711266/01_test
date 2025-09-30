eacd61266af1521511464308b8104427348c96a6
zhengshuang <zhengshuang@wps.cn>
2025-09-30 16:29:35 +0800

    增加定义包含成员函数的类

--- 02_class_memory_layout/include/class_types.h
+++ 02_class_memory_layout/include/class_types.h
@@ -12,3 +12,26 @@ private:
     double double_member; // 8 bytes
     bool bool_member; // 1 byte
 };
+
+// 3、包含成员函数的类
+class MemberFunctionClass
+{
+private:
+    int x;
+    int y;
+public:
+    void set_values(int a, int b) 
+    {
+        x = a; 
+        y = b;
+    }
+    int get_sum() const 
+    {
+        return x + y; 
+    }
+
+    static void static_function() 
+    {
+        // 静态成员函数，不占用对象内存
+    }
+};
